b7884619f034806445df3ed91958b2c5d3b17ded
Jeremy Soller <jeremy@system76.com>
2020-07-30 15:59:27 -0600

    WIP: kbled init

--- src/board/system76/bonw14/kbled.c
+++ src/board/system76/bonw14/kbled.c
@@ -1,11 +1,21 @@
 #include <board/kbled.h>
+#include <common/debug.h>
+#include <ec/i2c.h>
 #include <ec/pwm.h>
 
 void kbled_init(void) {
-    kbled_reset();
+    i2c_reset(&I2C_DGPU, true);
 }
 
 void kbled_reset(void) {
+    uint8_t value = 0xE4;
+    int res = i2c_set(&I2C_DGPU, 0x2D, 0xA0, &value, 1);
+    DEBUG("kbled_reset 0x2D: %d\n", res);
+
+    value = 0xC4;
+    res = i2c_set(&I2C_DGPU, 0x66, 0xA0, &value, 1);
+    DEBUG("kbled_reset 0x66: %d\n", res);
+
     // Set brightness and color
     kbled_set_color(0xFFFFFF);
     kbled_set(0x00);
